3abce162d3487cf3c7d170a7bc95affa14859ac6
jeebanKishore <jeebankishore332@gmail.com>
2019-05-31 15:00:32 +0530

    Show Control panel After all the fonts get loaded.

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 /////////////////////////////Variables///////////////////////////
-const textArea = document.getElementById('inputArea');
 const MAX_WIDTH = 500;
 const svgns = 'http://www.w3.org/2000/svg';
 const canvas = document.getElementById('cnv');
 const ctx = canvas.getContext('2d');
-const fontData = [{ "name": "Amatic-Bold", "value": "Amatic-Bold" }, { "name": "belligerent", "value": "belligerent" }, { "name": "Blokletters-Viltstift", "value": "Blokletters-Viltstift" }, { "name": "desyrel", "value": "desyrel" }, { "name": "justanotherhand-regular", "value": "justanotherhand-regular" }];
+const fontData = [{ 'name': 'Amatic-Bold', 'value': 'Amatic-Bold' }, { 'name': 'belligerent', 'value': 'belligerent' }, { 'name': 'Blokletters-Viltstift', 'value': 'Blokletters-Viltstift' }, { 'name': 'desyrel', 'value': 'desyrel' }, { 'name': 'justanotherhand-regular', 'value': 'justanotherhand-regular' }];
 const font = [];
 let fontsLoaded = 0;
 let totalFonts = 0;
+let textAreaValue = document.getElementById('inputArea').value;
 
+
+
+////////////////////////Public Functiones///////////////////////
 const continueLoadingFonts = (fontsLoaded, totalFonts) => {
     loadFont('assets/' + fontData[fontsLoaded].value + '.ttf');
 }
 
 const loadFont = (ttfpath) => {
-    opentype.load(ttfpath, function(err, _font) {
+    opentype.load(ttfpath, (err, _font) => {
         tempfont = _font;
         font.push(tempfont);
         fontsLoaded++;
         if (fontsLoaded < totalFonts) {
             continueLoadingFonts(fontsLoaded, totalFonts);
         } else {
-            //txtarea = document.getElementById('text_input');
-            //textgrp = document.getElementById('txtgrp');
-            //$scope.onTextChange();
+            document.getElementById('controlPanel').style.cssText = 'display: block';
         }
-
     });
+}
+
+const addTexts = () => {
+    alert('I am here.');
+    let numoflines = textAreaValue.split('\n').length - 1;
 }
 
 window.onload = () => {
