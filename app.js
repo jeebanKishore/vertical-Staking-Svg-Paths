d116bab92b0965f471c146c8efc40d4bbc714759
jeebanKishore <jeebankishore332@gmail.com>
2019-05-31 22:59:09 +0530

    Done Something else!

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 /////////////////////////////Variables///////////////////////////
 const MAX_WIDTH = 500;
 const svgns = 'http://www.w3.org/2000/svg';
-const canvas = document.getElementById('cnv');
-const ctx = canvas.getContext('2d');
-const fontData = [{ 'name': 'Amatic-Bold', 'value': 'Amatic-Bold' }, { 'name': 'belligerent', 'value': 'belligerent' }, { 'name': 'Blokletters-Viltstift', 'value': 'Blokletters-Viltstift' }, { 'name': 'desyrel', 'value': 'desyrel' }, { 'name': 'justanotherhand-regular', 'value': 'justanotherhand-regular' }];
+const fontData = [{ 'name': 'BlackOpsOne', 'value': 'BlackOpsOne' }, { 'name': 'charlotte', 'value': 'charlotte' }, { 'name': 'Great Times Font', 'value': 'Great Times Font' }, { 'name': 'StellaAlpina', 'value': 'StellaAlpina' }];
 const font = [];
-const FONT_SIZE = 72;
+const FONT_SIZE = 90;
 const inputTextArea = document.getElementById('inputArea');
 let fontsLoaded = 0;
 let totalFonts = 0;
@@ -18,7 +16,7 @@ let inputTextAreaValue = '';
 
 ////////////////////////Public Functiones///////////////////////
 const continueLoadingFonts = (fontsLoaded, totalFonts) => {
-    loadFont('assets/' + fontData[fontsLoaded].value + '.ttf');
+    loadFont('assets/' + fontData[fontsLoaded].value + '.otf');
 }
 
 const loadFont = (ttfpath) => {
@@ -36,32 +34,29 @@ const loadFont = (ttfpath) => {
 
 const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+const getLineNumber = () => inputTextAreaValue.substr(0, inputTextArea.selectionStart).split('\n').length;
+
 const addTexts = () => {
     //alert('I am here.');
     inputTextAreaValue = inputTextArea.value;
-    numoflines = inputTextAreaValue.split('\n').length - 1;
     if (inputTextAreaValue === '') inputTextAreaValue = ' ';
-    const allBbox = [];
-    tempBBox = measureText(inputTextAreaValue, FONT_SIZE, randomIntFromInterval(0, totalFonts - 1));
-    allBbox.push(tempBBox);
-
-}
-
-const getLineNumber = () => inputTextAreaValue.substr(0, inputTextArea.selectionStart).split('\n').length;
-
-const measureText = (text, fontSize, fontPositionNumber) => {
+    numoflines = inputTextAreaValue.split('\n').length - 1;
     const lineNumber = getLineNumber();
     textLineArray = inputTextAreaValue.split('\n');
     const textFromLineArray = textLineArray[lineNumber - 1];
-    let textPaths = font[fontPositionNumber].getPath(textFromLineArray, 0, fontSize * lineNumber, fontSize, { letterSpacing: letterSpacing });
+    const allBbox = [];
+    tempBBox = measureText(inputTextAreaValue, FONT_SIZE, randomIntFromInterval(0, totalFonts - 1), lineNumber, textFromLineArray);
+    allBbox.push(tempBBox);
+    console.log(allBbox);
+
+}
+
+
+const measureText = (text, fontSize, fontPositionNumber, lineNumber, textFromLineArray) => {
+    let tempselectedfont = font[fontPositionNumber];
+    let textPaths = tempselectedfont.getPath(textFromLineArray, 0, fontSize * lineNumber, fontSize, { letterSpacing: letterSpacing });
     const group = document.getElementById('txtgrp');
     group.innerHTML = '';
-    const scalerect = document.createElementNS(svgns, 'rect');
-    scalerect.setAttributeNS(svgns, 'x', 0);
-    scalerect.setAttributeNS(svgns, 'y', 0);
-    scalerect.setAttributeNS(svgns, 'fill', 'none');
-    scalerect.setAttributeNS(svgns, 'opacity', '0');
-    group.appendChild(scalerect);
     let pathString = '';
     let totalwidth = 0;
     let maxheight = 0;
@@ -82,7 +77,6 @@ const measureText = (text, fontSize, fontPositionNumber) => {
         }
     }
     const pathlen = textPaths.length / 2;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     mergedd = '';
     for (p = 0; p < textPaths.length; p++) {
         textPaths[p].fill = 'red';
@@ -96,14 +90,6 @@ const measureText = (text, fontSize, fontPositionNumber) => {
             rgb = Math.min(100 + Math.ceil(p * 150 / pathlen), 255);
         else
             rgb = Math.min(100 + Math.ceil((2 * pathlen - p) * 150 / (pathlen)), 255);
-        //textPaths[p].fill = 'rgb('+ rgb +','+ rgb +','+ 50 +')';
-        //textPaths[p].stroke = 'white';
-        //if($scope.strokeselected.indexOf('#') == -1)
-        //textPaths[p].stroke = 'url(#'+ $scope.strokeselected +')';
-        //else
-        textPaths[p].stroke = 'blue';
-        //textPaths[p].stroke = 'url(#gradientstroke)';
-        textPaths[p].strokeWidth = 5;
         textPaths[p].totalwidth = totalwidth;
         textPaths[p].maxheight = maxheight;
         textPaths[p].starty = starty;
@@ -113,9 +99,9 @@ const measureText = (text, fontSize, fontPositionNumber) => {
         mergedd += textPaths[p].toSVGPathd() + ' ';
     }
 
-    scalerect.setAttributeNS(svgns, 'width', maxheight);
-    scalerect.setAttributeNS(svgns, 'height', maxheight);
-    scalerect.setAttributeNS(svgns, 'y', starty);
+    //scalerect.setAttributeNS(svgns, 'width', maxheight);
+    //scalerect.setAttributeNS(svgns, 'height', maxheight);
+    //scalerect.setAttributeNS(svgns, 'y', starty);
     group.innerHTML += pathString.trim();
     const bbox = group.getBBox();
     return bbox;
